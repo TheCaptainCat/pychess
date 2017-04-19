571e0c7d142a133a00e8fb28e715d2f792ee090a
Pierre Chat <pierrechat89@hotmail.fr>
2017-04-19 19:18:57 +0200

    Début du gestionnaire de déplacements

---
+++ src/chess/computing/manager.py
@@ -0,0 +1,8 @@
+class Manager:
+    """Contains mechanics to move pieces on the chess board."""
+
+    def __init__(self, board):
+        self.board = board
+
+    def moves_list(self):
+        pass
